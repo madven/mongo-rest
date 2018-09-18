4c504f3d650ba702534457f5526ace907592185c
selcuk <selcukc@gmail.com>
2018-09-19 01:12:58 +0300

    pets removed

--- src/main/java/com/selcukc/mongo_rest/repositories/PetsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.selcukc.mongo_rest.repositories;
-
-import com.selcukc.mongo_rest.models.Pets;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PetsRepository extends MongoRepository<Pets, String> {
-	Pets findBy_id(ObjectId _id);
-}
